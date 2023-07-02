9877192d07d7857444c444b5be9174ce7ae7a904
Oleg <126164012+olreyplay@users.noreply.github.com>
2023-07-02 17:24:01 +0300

    1

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import Loader from "./Loader/Loader";
+
+const HomePage = lazy(() => import("./HomePage/HomePage"));
+const AboutPage = lazy(() => import("./AboutPage/AboutPage"));
+const ProductsPage = lazy(() => import("./ProductsPage/ProductsPage"));
+
+const App = () => {
+  return (
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default App;
---
+++ src/ProductsPage/ProductsPage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <header>
+        <h1>Our Products</h1>
+      </header>
+      <main>
+        <section>
+          <ul>
+            <li>Product One</li>
+            <li>Product Two</li>
+            <li>Product Three</li>
+          </ul>
+        </section>
+      </main>
+      <hr />
+      <footer>
+        <p>&copy; 2023 Rindull. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default ProductsPage;
