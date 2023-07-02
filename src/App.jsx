8caf95923cc09b045f1f2d6f03f51eafa4cb9cd3
Oleg <126164012+olreyplay@users.noreply.github.com>
2023-07-02 17:38:17 +0300

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Loader from "./Loader/Loader";
+import NavBar from "./NavBar/NavBar";
 
 const HomePage = lazy(() => import("./HomePage/HomePage"));
 const AboutPage = lazy(() => import("./AboutPage/AboutPage"));
@@ -9,6 +10,7 @@ const ProductsPage = lazy(() => import("./ProductsPage/ProductsPage"));
 const App = () => {
   return (
     <>
+      <NavBar />
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<HomePage />} />
---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+      </ul>
+      <hr />
+    </nav>
+  );
+};
+
+export default NavBar;
